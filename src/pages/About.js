c6f804ea6d95e652f76891db5364647220e6f3df
Neto Pompeu <neto1@hotmail.fr>
2021-05-06 17:40:45 +0200

    create component Navigation + style scss  component  + Navigation

---
+++ src/pages/About.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>About</h1>
+      <br />
+      <p>
+        Commodo consectetur officia ex eu amet cupidatat ex velit. Commodo
+        reprehenderit velit est minim est nostrud aliquip sint dolor nostrud ut.
+        Dolore laboris voluptate incididunt ullamco. Lorem ut eu laboris
+        excepteur esse mollit proident. Incididunt elit duis non ea. Officia
+        sunt ut veniam voluptate excepteur eiusmod ullamco dolore irure sit
+        incididunt aute velit. Id eiusmod dolor quis occaecat.
+      </p>
+      <br />
+      <p>
+        Cillum cillum reprehenderit sit labore enim. Tempor irure dolore culpa
+        irure Lorem adipisicing officia labore ea. Fugiat tempor amet ex duis
+        est et ullamco cillum amet culpa.
+      </p>
+    </div>
+  );
+};
+
+export default About;
